c151783f0e19d31ac3c4e9918fa2178673609c14
Isaac Custódio <54863283+SaykonDesu@users.noreply.github.com>
2020-10-22 16:36:28 -0300

    configuração e implementação do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCJkKLD3HCGovUS7NQrDvfjMIBWfIVglbk",
+    authDomain: "crud-ionic-estudo.firebaseapp.com",
+    databaseURL: "https://crud-ionic-estudo.firebaseio.com",
+    projectId: "crud-ionic-estudo",
+    storageBucket: "crud-ionic-estudo.appspot.com",
+    messagingSenderId: "817665971462",
+    appId: "1:817665971462:web:581e70f2fa82c9fc21fa87",
+    measurementId: "G-VSW923VEQT"
+  }
 };
